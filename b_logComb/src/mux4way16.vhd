7848d1529b6f2486466f84c066737879eedef955
Marcos <marcosgraf@al.insper.edu.br>
2025-09-10 16:26:16 -0300

    mux4way16

--- b_logComb/src/mux4way16.vhd
+++ b_logComb/src/mux4way16.vhd
@@ -2,23 +2,19 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux4way16 is
-    port ( 
-        a   : in  STD_LOGIC_VECTOR(15 downto 0);
-        b   : in  STD_LOGIC_VECTOR(15 downto 0);
-        c   : in  STD_LOGIC_VECTOR(15 downto 0);
-        d   : in  STD_LOGIC_VECTOR(15 downto 0);
-        sel : in  STD_LOGIC_VECTOR(1 downto 0);
-        q   : out STD_LOGIC_VECTOR(15 downto 0)
-    );
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			c:   in  STD_LOGIC_VECTOR(15 downto 0);
+			d:   in  STD_LOGIC_VECTOR(15 downto 0);
+			sel: in  STD_LOGIC_VECTOR(1 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
 
 architecture arch of mux4way16 is
 begin
-	
-    with sel select
-        q <= a when "00",
-             b when "01",
-             c when "10",
-             d when "11";
-
+	q <= a when sel = "00" else
+		 b when sel = "01" else
+		 c when sel = "10" else
+		 d;
 end architecture;
